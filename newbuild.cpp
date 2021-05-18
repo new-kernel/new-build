445a8cb36e03ad7b3d3c1a42f6fc15cbb761790e
NathanMcMillan54 <nathanmcmillan54@gmail.com>
2021-05-18 18:10:46 -0400

    Added print function.

--- newbuild.cpp
+++ newbuild.cpp
@@ -13,6 +13,10 @@ void ld(string arg) {
     system(command);
 }
 
+void print(string text) {
+    cout << text << endl;
+}
+
 int main() {
     ifstream file;
     file.open("NewBuild");
@@ -32,11 +36,13 @@ int main() {
                 static const map<string, void (*)(string)> gcc_fn = { {"gcc", gcc} };
                 static const map<string, void (*)(string)> as_fn = { {"as", as} };
                 static const map<string, void (*)(string)> ld_fn = { {"ld", ld} };
+                static const map<string, void (*)(string)> print_fn = { {"print", print} };
 
                 map<string, void (*)(string)>::const_iterator cargo_arg = cargo_fn.find(fn);
                 map<string, void (*)(string)>::const_iterator gcc_arg = gcc_fn.find(fn);
                 map<string, void (*)(string)>::const_iterator as_arg = as_fn.find(fn);
                 map<string, void (*)(string)>::const_iterator ld_arg = ld_fn.find(fn);
+                map<string, void (*)(string)>::const_iterator print_arg = print_fn.find(fn);
 
                 if ((cargo_arg = cargo_fn.find(fn)) != cargo_fn.end()) {
                     if (((p1 = line.find('"', p1+1)) == string::npos) ||
@@ -62,6 +68,12 @@ int main() {
                         cout << "No string argument found in " << line << endl;
                     else
                         ld_arg -> second(line.substr(p1 + 1, p2 - p1 - 1));
+                } else if ((print_arg = print_fn.find(fn)) != print_fn.end()) {
+                    if (((p1 = line.find('"', p1 + 1)) == string::npos) ||
+                        ((p2 = line.find('"', p1 + 1)) == string::npos))
+                        cout << "No string argument found in " << line << endl;
+                    else
+                        print_arg -> second(line.substr(p1 + 1, p2 - p1 - 1));
                 } else {
                     cout << "Unknown function " << fn << endl;
                 }
